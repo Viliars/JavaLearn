81c07c4b91d705f5f47507d17ebcbeea12fb1257
m.pritugin <m.pritugin@corp.mail.ru>
2019-07-31 18:08:12 +0300

    Task 4A

---
+++ src/Task_4A.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Task_4A {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            int w = Integer.parseInt(reader.readLine());
+            if(w != 2 && w%2==0) {
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+    }
+}
